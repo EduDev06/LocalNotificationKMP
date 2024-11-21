7dde0e95506ec2f0769067d5cad764d733f5cb2a
paucar <bpaucar@proveedor.intercorp.com.pe>
2024-11-21 11:34:26 -0500

    Initial Project

---
+++ composeApp/src/commonMain/kotlin/org/sample/localnotificationmultiplatform/App.kt
@@ -0,0 +1,14 @@
+package org.sample.localnotificationmultiplatform
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Text("Hi")
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/org/sample/localnotificationmultiplatform/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.sample.localnotificationmultiplatform
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
